d9f78329d48f828cbcb3690efcec3dab0ced9553
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-12-14 15:51:20 -0600

    Create Container.tsx

---
+++ components/Container.tsx
@@ -0,0 +1,11 @@
+export function Container({
+  className = "",
+  children
+}: {
+  className?: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={`mx-auto w-full max-w-6xl px-4 ${className}`}>{children}</div>
+  );
+}
